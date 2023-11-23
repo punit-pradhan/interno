58a8653dc0a344ccfbb652db53c214ad8519b64c
punit pradhan <punit.pradhan@innoraft.com>
2023-11-23 14:05:58 +0530

    third commit

--- jquery/navbar.js
+++
@@ -1,7 +0,0 @@
-jQuery.noConflict();
-jQuery(document).ready(function($) {
-   // Your code using $ as an alias for jQuery
-   $('#menu-acces').on('click', function (e) {
-      $(".hidden-menu").toggleClass('show');
-   });
-});
